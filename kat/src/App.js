53f36fd3c737903632581fc1a1840205557dd0e1
Corinne Padilla <corinne padilla>
2022-08-10 06:36:38 -0700

    copied styles.css and about.css over to index.css and index2.css

--- kat/src/App.js
+++ kat/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import './about.css';
 
-function App() {
+const styles = {
+ 
+}
+
+const about = {
+  
+}
+
+function css() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+       hello world
       </header>
     </div>
   );
 }
 
-export default App;
+export default css;
