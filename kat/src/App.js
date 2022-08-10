5610b20de1ed36d855838786bd905be04d33d40a
Corinne Padilla <corinne padilla>
2022-08-10 07:21:19 -0700

    added index2html to app.js(so far broken)

--- kat/src/App.js
+++ kat/src/App.js
@@ -1,25 +1,129 @@
 import './index2.css';
 import './index.css.';
-import  './about.html';
-import 'ashtanga.html';
-import 'bikram.html';
-import 'contact.html';
-import 'hatha.html';
-import 'index2.html';
-import 'iyengar.html';
-import 'kundalini.html';
-import 'restorative.html';
-import 'vinyasa.html';
-import 'yin.html';
+import './about.html';
+import './ashtanga.html';
+import './bikram.html';
+import './contact.html';
+import './hatha.html';
+import './index2.html';
+import './iyengar.html';
+import './kundalini.html';
+import './restorative.html';
+import './vinyasa.html';
+import './yin.html';
+
+
+// const index2 = ./indexedDB.cmp(index2, {
+//   ALLOWED_TAGS: ["body", "h1", "div", "a", "header", "div", "img", "footer"],
+//   ALLOWED_ATTR: ["style"],
+// });
+
+const index2 = ./index2.html(index2, {
+  ALLOWED_TAGS: ["body", "h1", "div", "a", "header", "div", "img", "footer"],
+  ALLOWED_ATTR: ["style"],
+}); [ 
+ [<body>
+
+[<a name="#"></a>]
+
+    <div class="topnav" id="myTopnav">
+        <a class="split" href="index.html"><img class="logo" src="img/logo.jpg" alt="logo"
+                style="width: 100px; height: 60px;"/></a>
+        <a href="index.html" class="active">Home</a>
+        <a href="about.html">Classes We Offer</a>
+        <a href="contact.html">Contact</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>
+
+    <header>
+        <h1>Jane Smith Inc.</h1>
+    </header>
+
+    <div class="parallax">
+        <div class="container container-fluid mt-3">
+            <div class="mt-4 p-5 bg-transparent text-white rounded">
+                <h1>Studio 1</h1>
+                <img src="img/yogastudio.jpg" alt="yogaclass3" width="100%"/>
+                <p class="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip
+                    ex ea commodo consequat..</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="parallax">
+        <div class="container container-fluid mt-3">
+            <div class="mt-4 p-5 bg-transparent text-white rounded">
+                <h1>Studio 2</h1>
+                <img src="img/yogastudio2.jpg" alt="yogaclass3" width="100%"/>
+                <p class="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip
+                    ex ea commodo consequat..</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="parallax">
+        <div class="container container-fluid mt-3">
+            <div class="mt-4 p-5 bg-transparent text-white rounded">
+                <h1>Studio 3</h1>
+                <img src="img/yogastudio3.avif" alt="yogaclass3" width="100%"/>
+                <p class="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip
+                    ex ea commodo consequat..</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="parallax" style="height: 100%;">
+        <div class="container container-fluid mt-3">
+            <div class="mt-4 p-5 bg-transparent text-white rounded">
+                <h1>Studio 4</h1>
+                <img src="img/yogastudio4.jpg" alt="yogaclass3" width="100%"/>
+                <p class="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip
+                    ex ea commodo consequat..</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="parallax"></div>
+
+
+    <footer>
+        <a class="split2" href="index.html"><img class="logo" src="img/logo.jpg" alt="logo"
+                style="width: 100px; height: 60px;"/></a>
+        <p class="text-light">&copy; 2022 | Jane Smith Inc.</p>
+        <p class="text-light">Contact Number: 123.456.7890</p>
+        <p class="text-light">Email Address: example@example.com</p>
+        <p class="text-light">Location Address: 123 Example Street Ste 0 Example, State 00000</p>
+        <a class="backtotop" href="#">Back to Top</a>
+    </footer>
+</body>
+
+]];
+
 
 
 
 function App() {
   return (
     <div>
-      <header>
-       hello world
-      </header>
+      <header>hello world</header>
     </div>
   );
 }
